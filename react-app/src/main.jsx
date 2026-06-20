a9f46952929f0b3de1b2f1bd0285977ca49a545b
Mouli1912 <19moulidhara@gmail.com>
2026-06-20 22:12:36 +0530

    learn react jsx expression

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -15,3 +15,48 @@ const myElement = (
     <li>Cherries</li>
   </ul>
 );
+
+// react expression
+
+function Car (){
+  return (
+  <>
+ <h1>My Car</h1>
+ <p>It has {218 * 1.36} horsepower</p>
+
+  </>
+  );
+
+  
+
+}
+
+//function call
+
+function Kwtohp (kw){
+  return kw * 1.36;
+
+}
+ function Car(){
+  return (
+    <>
+    <h1> My car</h1>
+    <p>It has {Kwtohp(218)} horsepower</p>
+    </>
+  )
+ }
+
+ // object properties
+
+ function Car (){
+  const myobj = {
+    name:"Ford Mustang",
+    model:"500",
+    color:"Black"
+  };
+  return (
+    <>
+    <h1>My Car is {myobj.color} {myobj.name} {myobj.model}</h1>
+    </>
+  );
+ }
